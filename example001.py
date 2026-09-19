3b1f84d6e11312af2fcf64b538f77245b10fed06
Yoel Aguilar <joelaguilar200305@gmail.com>
2026-09-18 19:59:17 -0500

    cree mi primer Hola mundo y cree mi primer calculadora

---
+++ example001.py
@@ -0,0 +1,14 @@
+# print("¡Hola mundo")
+
+Poder1 = int(input("Ingrese el primer numero: "))
+Poder2 = int(input("ingrese el segundo numero: "))
+
+suma = Poder1 + Poder2
+resta = Poder1 - Poder2
+multiplicacion = Poder1 * Poder2
+division = Poder1 / Poder2
+
+print("El resultado de la suma es:", suma )
+print("El resultado de la resta es:", resta)
+print("El resultado de la multipliucacion es:", multiplicacion)
+print("El resultado de la division es:", division)
