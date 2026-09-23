0896d9ec718d44ffb11e2249b950bb4c81a8e3fa
Yoel Aguilar <joelaguilar200305@gmail.com>
2026-09-22 19:10:51 -0500

    cree mi nombre con luces

---
+++ example003.py
@@ -0,0 +1,23 @@
+#Nombre con luces
+
+nombre = input("¿como te llamas?: ") # es el mensaje que se pregunta al usuario
+print (f"El expresso de cardiff {nombre}")
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ example004.py
@@ -0,0 +1,10 @@
+#Tabla de multiplicar 5, Ninja
+
+tabla_de_5 = 5 
+#el bucle for 
+for i in range(1, 11):
+    result = tabla_de_5 * i 
+    print (f"{tabla_de_5} x {i} = {result}") #f se sirve para meter variable dentro del texto 
+
+    
+
